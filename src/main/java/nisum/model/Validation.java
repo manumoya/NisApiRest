a79f0d52c173d2ff725edd7affa04117f85ee9f2
Manuel Moya <mmoyam@gmail.com>
2019-11-21 07:13:32 -0300

    Valida hairColour
    hairColour  - Permitidos: 'black', 'red', 'yellow', 'brown' & 'white'"

--- src/main/java/nisum/model/Validation.java
+++ src/main/java/nisum/model/Validation.java
@@ -1,4 +1,32 @@
 package nisum.model;
 
 public class Validation {
+
+    private boolean validated;
+    private String message;
+
+    public Validation(){
+    }
+
+    public Validation(boolean validated, String message) {
+        this.validated = validated;
+        this.message = message;
+    }
+
+    public boolean isValidated() {
+        return validated;
+    }
+
+    public void setValidated(boolean validated) {
+        this.validated = validated;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
